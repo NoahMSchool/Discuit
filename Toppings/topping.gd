35a24064995d966a117907a56501c2c8c1b2c519
Jon Marks <jon@kaldorgroup.com>
2025-08-29 18:26:00 +0100

    Preload the images so they work in the export

--- Toppings/topping.gd
+++ Toppings/topping.gd
@@ -3,12 +3,17 @@ extends Area3D
 #@export var topping_num = 0
 
 @onready var topping_meshes = [$MeshContainer/Blueberry, $MeshContainer/GumDrop, $MeshContainer/Chocolate, $MeshContainer/Almond, $MeshContainer/Acorn]
-@onready var topping_images = [Image.load_from_file("res://Toppings/blueberryIcon.png"), 
-								Image.load_from_file("res://Toppings/gumdrop_icon.png"), 
-								Image.load_from_file("res://Toppings/ChocolateIcon.png"), 
-								Image.load_from_file("res://Toppings/AlmondIcon.png"), 
-								Image.load_from_file("res://Toppings/AcornIcon.png")
-								]
+
+
+
+@onready var topping_images: Array[Image] = [
+	(preload("res://Toppings/blueberryIcon.png") as Texture2D).get_image(),
+	(preload("res://Toppings/gumdrop_icon.png") as Texture2D).get_image(),
+	(preload("res://Toppings/ChocolateIcon.png") as Texture2D).get_image(),
+	(preload("res://Toppings/AlmondIcon.png") as Texture2D).get_image(),
+	(preload("res://Toppings/AcornIcon.png") as Texture2D).get_image(),
+]
+
 
 @onready var topping_num = randi_range(0,topping_meshes.size()-1)
 
